50aedfcd2ee023037c7c4a8a829dcd5d0c8812e9
Paulo011 <pauloortega011@gmail.com>
2022-09-17 13:40:57 -0300

    correction in element h3

--- client/src/view/components/timer.js
+++ client/src/view/components/timer.js
@@ -1,6 +1,6 @@
 class Timer {
   constructor(className, innerText){
-    this.element = document.createElement(h3)
+    this.element = document.createElement("h3")
     this.element.className = className
     this.element.innerText = innerText
   }
